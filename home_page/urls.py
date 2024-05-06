8a84ca953b66d803cefebe9a4d8f2f577a1c330e
satcomjimmy <33491206+satcomjimmy@users.noreply.github.com>
2024-05-05 23:10:38 -0400

    Changes base index page.

--- home_page/urls.py
+++ home_page/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('meal_plan/', include('meal_plan.urls')),
-    # path('', include('meal_plan.urls')),
+    # path('meal_plan/', include('meal_plan.urls')),
+    path('', include('meal_plan.urls')),
 ]
